3f47e96a0ea2f1a1a4254607c43f0f0a1b4eaf34
Sunny Dodti <sunnydodti@gmail.com>
2025-05-24 16:47:25 +0530

    add error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import routes from "./routes";
 import dotenv from 'dotenv';
+import { errorMiddleware } from "./middlewares/errorMiddleware"
 dotenv.config();
-const app=new Hono();
-app.route("/api",routes);
+const app = new Hono();
+app.route("/api", routes);
+app.onError(errorMiddleware);
 export default app;
---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,15 @@
+import { Context } from 'hono';
+import { AuthError, DatabaseError, PermissionError, RequestError, UnprocessableError } from '../errors';
+
+export const errorMiddleware = (err: any, c: any) => {
+
+    if (err instanceof AuthError) return c.json({ error: err.message }, err.statusCode);
+    if (err instanceof RequestError) return c.json({ error: err.message }, err.statusCode);
+    if (err instanceof PermissionError) return c.json({ error: err.message }, err.statusCode);
+    if (err instanceof DatabaseError) return c.json({ error: err.message }, err.statusCode);
+    if (err instanceof UnprocessableError) return c.json({ error: err.message }, err.statusCode);
+
+    console.error(err.stack);
+    c.json({ error: 'Something went wrong!', details: err.message }, 500);
+};
+
