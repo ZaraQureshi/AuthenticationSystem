d56a89b40b48f945a094733f115db5edf3849fae
ZaraQureshi <qzara819@gmail.com>
2025-05-26 10:54:36 +0530

    add authMiddleware to src/middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import { Hono } from 'hono';
 import { register } from 'module';
-import { registerUser,ping,loginUser,refreshToken,forgotPassword, resetPassword, purgeExpiredTokens } from '../src/contoller';
-import { emailVerification } from './middleware/authMiddleware';
+import { registerUser,ping,loginUser,refreshToken,forgotPassword, resetPassword, purgeExpiredTokens, logout } from '../src/contoller';
+import { emailVerification } from './middlewares/authMiddleware';
  const routes = new Hono();
 
 routes.post('/register',registerUser);
 routes.post('/login',loginUser);
-routes.post('/logout');
+routes.post('/logout',logout);
 routes.post('/refresh-token',refreshToken);
 routes.post('/forgot-password',emailVerification,forgotPassword);
 routes.post('/reset-password',resetPassword);
