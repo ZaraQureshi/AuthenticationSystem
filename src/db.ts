aea65b1a6937835d0099b9d3c3e032b21b6e798e
ZaraQureshi <qzara819@gmail.com>
2025-05-29 12:28:22 +0530

    create functionality for multi database support

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,28 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from '../src/drizzle/schema';
+import * as pgSchema from '../src/drizzle/schema';
+import * as mysqlSchema from '../src/drizzle/mysqlSchema'; // example
+import mysql from 'mysql2/promise';
+
 
 import dotenv from 'dotenv';
 dotenv.config();
 
+export const createSchema=async (dbType:string,connectionString:string)=>{
+  let createDb;
+  if(dbType='postgres'){
+      let pool=new Pool({connectionString})
+      createDb=drizzle(pool,{schema:pgSchema});
+  }else if (dbType === 'mysql') {
+    const connection = await mysql.createConnection(connectionString);
+    createDb = drizzle(connection, { schema: mysqlSchema });
+  } else {
+    throw new Error('Unsupported DB type');
+  }
+}
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  
+
 });
 
-export const db = drizzle(pool, { schema });
+// export const db = drizzle(pool, { schema });
