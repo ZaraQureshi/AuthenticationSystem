7a19f3753e68ad7914aed4a20936c44cdb1ac6c4
ZaraQureshi <qzara819@gmail.com>
2025-06-04 13:34:15 +0530

    included schema based on db

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
-import { createSchema} from "./db"
-import { users } from "./drizzle/schema";
+import { createSchema } from "./db"
+import { users as pgUser } from "./drizzle/schema";
+import { users as mysqlUser} from "./drizzle/mysqlSchema";
+
 import { and, eq } from "drizzle-orm";
 import { refreshToken } from "./contoller";
-import {Pool as PgPool} from 'pg';
+import { Pool as PgPool } from 'pg';
 import { drizzle } from "drizzle-orm/node-postgres";
 import { pgSchema } from "drizzle-orm/pg-core";
 import { create } from "domain";
@@ -11,42 +13,42 @@ import { create } from "domain";
 
 
 export const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(4),
+    email: z.string().email(),
+    password: z.string().min(4),
 });
 
 export const logoutSchema = z.object({
-  userId: z.number(),
-  refreshToken: z.string(),
+    userId: z.number(),
+    refreshToken: z.string(),
 });
 
 export const purgeExpiredTokensSchema = z.object({
-  secret: z.string().min(16),
+    secret: z.string().min(16),
 });
- 
+
 export const getUserByEmail = async (email: any) => {
-    const {db,type}= await createSchema();
-if(type==='postgres'){
+    const { db, type } = await createSchema();
+    
+    if (type === 'postgres') {
 
-    return await db.select().from(db._.schema.users).where(eq(users.email, email));
-}else if(type==='mysql'){
-    return await db.select().from(db._.schema.users).where(eq(users.email, email));
+        return await db.select().from(pgUser).where(eq(pgUser.email, email));
+    } else if (type === 'mysql') {
+        return await db.select().from(mysqlUser).where(eq(mysqlUser.email, email));
 
-}
+    }
 }
 
 export const getUserById = async (id: any) => {
-    const db= await createSchema();
+    const db = await createSchema();
 
-  return await db.select().from(users).where(eq(users.id, id));
+    return await db.select().from(users).where(eq(users.id, id));
 }
 
 // get jwt expiry time as date from the actual string token
 export const getExpiryFromToken = (token: string) => {
-  const payload = token.split(".")[1];
-  const decodedPayload = JSON.parse(atob(payload));
-  return new Date(decodedPayload.exp * 1000).toISOString();
+    const payload = token.split(".")[1];
+    const decodedPayload = JSON.parse(atob(payload));
+    return new Date(decodedPayload.exp * 1000).toISOString();
 }
 
 
- 
