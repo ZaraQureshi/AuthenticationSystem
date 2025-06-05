aef1b549bd9309b4f84df5e02a83b5c868bb2772
Sunny Dodti <sunnydodti@gmail.com>
2025-06-05 13:39:14 +0530

    remve dist dir

--- dist/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-export default ({
-    schema: './drizzle/schema.ts',
-    out: './drizzle/migrations',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
-    },
-    dialect: "postgresql"
-});
--- dist/drizzle/schema.js
+++
@@ -1,8 +0,0 @@
-import { pgTable, text, serial } from "drizzle-orm/pg-core";
-export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    username: text("name").notNull(),
-    email: text("email").notNull().unique(),
-    password: text("password").notNull(),
-    role: text("role").notNull().default("user"),
-});
