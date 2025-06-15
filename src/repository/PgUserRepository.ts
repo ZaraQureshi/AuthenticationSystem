e1b97da4d238d057aef75448b8a543a73d05c487
ZaraQureshi <qzara819@gmail.com>
2025-06-15 22:43:33 +0530

    add GetAllUsers method to PgUserrepository

---
+++ src/repository/PgUserRepository.ts
@@ -0,0 +1,14 @@
+import { users } from "../drizzle/schema";
+import { UserDTO } from "../model/User";
+import { IUserRepository } from "./IUserRepository";
+
+export class PgUserRepository implements IUserRepository{
+    constructor(private db: any) {}
+    async InsertUser(user:UserDTO){
+       return await this.db.insert(users).values({ user }).returning();
+    }
+
+    async GetAllUsers(){
+        return await this.db.select().from(users);
+    }
+}
