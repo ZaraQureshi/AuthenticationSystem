ba99199fd7d55742a02f25ac6661885cf1eceac6
ZaraQureshi <qzara819@gmail.com>
2025-06-21 19:50:49 +0530

    add async method to call initializeRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Context, Hono } from "hono";
-import routes from "./routes";
+import routes, { initializeRoutes } from "./routes";
 import dotenv from 'dotenv';
+
 import { errorMiddleware } from "./middlewares/errorMiddleware"
 dotenv.config();
 const app = new Hono();
+(async () => {
+await initializeRoutes();
 app.route("/api", routes);
 app.onError(errorMiddleware);
+})();
 export default app;
