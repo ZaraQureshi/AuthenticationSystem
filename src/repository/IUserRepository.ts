31a749868c2d6b399f5b26aed6be3dda9a8e899b
ZaraQureshi <qzara819@gmail.com>
2025-06-23 17:02:07 +0530

    modify insertUsers method

--- src/repository/IUserRepository.ts
+++ src/repository/IUserRepository.ts
@@ -5,4 +5,5 @@ import { UserDTO } from "../model/User";
 export interface IUserRepository {
     GetAllUsers(): Promise<any>;
     InsertUser(user: UserDTO): Promise<any>;
+    GetUserByEmail(email:string):Promise<any>
 }
--- src/repository/PgUserRepository.ts
+++ src/repository/PgUserRepository.ts
@@ -8,7 +8,14 @@ import { eq } from "drizzle-orm";
 export class PgUserRepository implements IUserRepository{
     constructor(@inject('Database') private db: any) {}
     async InsertUser(user:UserDTO){
-       return await this.db.insert(users).values({ user }).returning();
+       return await this.db.insert(users).values({
+        username: user.username,
+        email: user.email,
+        hashedPassword: user.password, // Use hashedPassword
+        role: user.role || 'user', // Default role
+        isBlocked: user.isBlocked || false, // Default: not blocked
+        isVerified: user.isVerified || false, // Default: not verified
+    }).returning();
     }
 
     async GetAllUsers(){
@@ -18,4 +25,6 @@ export class PgUserRepository implements IUserRepository{
     async GetUserByEmail(email: string): Promise<any> {
         return await this.db.select().from(users).where(eq(users.email,email))
     }
+
+    
 }
