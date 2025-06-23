9009d81f1ffc4a811ff411823ebd6fbc3e858f6a
ZaraQureshi <qzara819@gmail.com>
2025-06-23 22:31:48 +0530

    add refreshToken and forgotPAssword

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,15 @@
 import { inject, injectable } from "tsyringe";
 import { UserService } from "../service/UserService";
 import { Context } from "hono";
+import { sign, verify } from "jsonwebtoken";
+import { ACCESS_SECRET, generateAccessToken, REFRESH_SECRET } from "../utils";
 @injectable()
 export class UserController {
     constructor(@inject('UserService') private userService: UserService
     ) {
 
     }
+
     getAllUsers = async () => {
         try {
             const users = await this.userService.GetAllUsers();
@@ -34,4 +37,30 @@ export class UserController {
             console.log(e);
         }
     }
+    refreshToken = async (c: Context) => {
+        const { refreshToken } = await c.req.json();
+        try {
+            const payload = verify(refreshToken, REFRESH_SECRET);
+            const accessToken = generateAccessToken(payload);
+            return c.json({ accessToken })
+        }
+        catch (ex) {
+            return c.json({ "message": "Invalid refresh token" }, 401)
+        }
+    }
+
+    forgotPassword = async (c: Context) => {
+        const { email } = await c.req.json();
+        try {
+            const user = await this.userService.GetUserByEmail(email);
+            if (user.length === 1) {
+                const resetToken = sign({ email }, ACCESS_SECRET, { expiresIn: '1h' })
+                return c.json({ message: "If you have registered, you will recieve an email", resetToken });
+            }
+            return c.json({message:"Email does not exist"})
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
 }
