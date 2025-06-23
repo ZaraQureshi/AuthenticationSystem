8d23afa3f598bce64ccb7db59ec522884c6adf8d
ZaraQureshi <qzara819@gmail.com>
2025-06-23 17:01:33 +0530

    add register controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,37 @@
+import { inject, injectable } from "tsyringe";
 import { UserService } from "../service/UserService";
-
+import { Context } from "hono";
+@injectable()
 export class UserController {
-    constructor(private userService:UserService){
+    constructor(@inject('UserService') private userService: UserService
+    ) {
 
     }
-
-    getAllUsers=async ()=>{
-        try{
-            const users=await this.userService.getAllUsers();
+    getAllUsers = async () => {
+        try {
+            const users = await this.userService.GetAllUsers();
             console.log(users)
         }
-        catch(e){
+        catch (e) {
             console.log(e)
+        }
+    }
+    login = async (c: Context) => {
+        try {
+            const login = await this.userService.Login(c);
+            return c.json({ message: login })
+        } catch (e) {
+            console.log(e)
+
+        }
+    }
+    register = async (c: Context) => {
+        const { username, email, password } = await c.req.json();
+        try {
+            const registeredUser = await this.userService.Register(username, email, password);
+
+        } catch (e) {
+            console.log(e);
         }
     }
 }
