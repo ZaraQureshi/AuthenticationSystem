4aeaad2ce9e01ae1a850265dd1df340e3a56531e
ZaraQureshi <qzara819@gmail.com>
2025-06-30 22:08:25 +0530

    add DeleteToken, InsertToken, GetByToken

--- src/repository/IUserRepository.ts
+++ src/repository/IUserRepository.ts
@@ -1,9 +1,14 @@
 import { injectable } from "tsyringe";
 import { UserDTO } from "../model/User";
+import { TokenDTO } from "../model/Token";
 
 
 export interface IUserRepository {
     GetAllUsers(): Promise<any>;
     InsertUser(user: UserDTO): Promise<any>;
-    GetUserByEmail(email:string):Promise<any>
+    GetUserByEmail(email:string):Promise<any>;
+    UpdatePassword(email:string,password:string):Promise<any>;
+    GetByToken(token:string):Promise<any>;
+    InsertToken(token:TokenDTO):Promise<any>;
+    DeleteToken(expiryDate:string):Promise<any>;
 }
