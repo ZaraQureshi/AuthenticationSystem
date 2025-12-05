0c1acc6c88f29a5a78dd96cbd1b11b140c6e6f0b
ZaraQureshi <qzara819@gmail.com>
2025-12-05 21:18:58 +0530

    Remove dist from tracking

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import { Hono } from "hono";
-import routes from "./routes";
-import dotenv from 'dotenv';
-dotenv.config();
-const app = new Hono();
-app.route("/api", routes);
-export default app;
