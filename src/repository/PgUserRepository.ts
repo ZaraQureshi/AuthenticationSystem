11470c0c00c05cb56e34b5fe61420a3837662977
ZaraQureshi <qzara819@gmail.com>
2025-12-29 19:39:34 +0530

    made changes

--- src/repository/PgUserRepository.ts
+++ src/repository/PgUserRepository.ts
@@ -1,98 +1,78 @@
 import { inject, injectable } from "tsyringe";
 import { Kysely } from "kysely";
 import { Database } from "../model/Database";
-import { IUserRepository } from "./IUserRepository.ts";
-import { UserDTO } from "../model/User.ts";
-import { TokenDTO } from "../model/Token.ts";
-import { Pool } from "pg";
+import { IUserRepository } from "./IUserRepository";
+import { UserDTO } from "../model/User";
+import { TokenDTO } from "../model/Token";
 
 @injectable()
 export class PgUserRepository implements IUserRepository {
-    constructor(
-        @inject("Database") private db: Pool
-    ) { }
-    GetAllUsers(): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    UpdatePassword(email: string, password: string): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    GetByToken(token: string): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    InsertToken(token: TokenDTO): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    DeleteToken(): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    MigrateDB(): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
+  constructor(
+    @inject("Database") private db: Kysely<Database>
+  ) {}
 
-    // GetAllUsers() {
-    //     return this.db
-    //         .selectFrom("users")
-    //         .selectAll()
-    //         .execute();
-    // }
+  async GetAllUsers() {
+    return await this.db
+      .selectFrom("users")
+      .selectAll()
+      .execute();
+  }
 
-    GetUserByEmail(email: string) {
-        return this.db.query(
-            `SELECT * FROM users WHERE email = '${email}'`
-        )
-    }
+  async GetUserByEmail(email: string) {
+    return await this.db
+      .selectFrom("users")
+      .selectAll()
+      .where("email", "=", email)
+      .execute();
+  }
 
-    InsertUser(user: UserDTO) {
-        console.log("Inserting user:", user);
-        return this.db.query(`insert into users (username, email, password, role, isBlocked, isVerified) values
-            ('${user.username}', '${user.email}', '${user.password}', '${user.role}', ${user.isBlocked}, ${user.isVerified}) returning *`);
-        // return this.db
-        //     .insertInto("users")
-        //     .values({
-        //         username: user.username,
-        //         email: user.email,
-        //         password: user.password,
-        //         role: user.role,
-        //         isBlocked: user.isBlocked,
-        //         isVerified: user.isVerified,
-        //     })
-        //     .returningAll()
-        //     .executeTakeFirst();
-    }
+  async InsertUser(user: UserDTO) {
+    return await this.db
+      .insertInto("users")
+      .values({
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        role: user.role,
+        isBlocked: user.isBlocked,
+        isVerified: user.isVerified,
+      })
+      .returning("id")
+      .executeTakeFirst();
+  }
 
-    // UpdatePassword(email: string, hashedPassword: string) {
-    //     return this.db
-    //         .updateTable("users")
-    //         .set({ password: hashedPassword })
-    //         .where("email", "=", email)
-    //         .executeTakeFirst();
-    // }
+  UpdatePassword(email: string, hashedPassword: string) {
+    return this.db
+      .updateTable("users")
+      .set({ password: hashedPassword })
+      .where("email", "=", email)
+      .executeTakeFirst();
+  }
 
-    // GetByToken(token: string) {
-    //     return this.db
-    //         .selectFrom("tokens")
-    //         .selectAll()
-    //         .where("token", "=", token)
-    //         .execute();
-    // }
+  GetByToken(token: string) {
+    return this.db
+      .selectFrom("tokens")
+      .selectAll()
+      .where("token", "=", token)
+      .execute();
+  }
 
-    // InsertToken(token: TokenDTO) {
-    //     return this.db
-    //         .insertInto("tokens")
-    //         .values(token)
-    //         .executeTakeFirst();
-    // }
+  InsertToken(token: TokenDTO) {
+    return this.db
+      .insertInto("tokens")
+      .values(token)
+      .executeTakeFirst();
+  }
 
-    // DeleteToken() {
-    //     return this.db
-    //         .deleteFrom("tokens")
-    //         .where("expiryDate", "<=", new Date().toISOString())
-    //         .execute();
-    // }
+  DeleteToken() {
+    return this.db
+      .deleteFrom("tokens")
+      .where("expiryDate", "<=", new Date().toISOString())
+      .execute();
+  }
 
-    // async MigrateDB() {
-    //     // optional – usually done outside auth packages
-    //     return true;
-    // }
+  async MigrateDB() {
+    // optional – usually done outside auth packages
+    return true;
+  }
 }
